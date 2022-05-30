a1206ce7ecfa1787a930122360f9f2728f941a5f
Jeroen Vermeylen <jeroen@vermeylen.org>
2022-05-30 21:45:04 +0200

    add scroll script

--- assets/izabo.js
+++ assets/izabo.js
@@ -12,6 +12,11 @@ const debounce = (fn) => {
 
 const storeScroll = () => {
   document.documentElement.dataset.scroll = window.scrollY;
+  if window.scrollY > 720 {
+    document.documentElement.dataset.bastheader = true;
+  } else {
+    document.documentElement.dataset.bastheader = false;
+  }
 }
 
 document.addEventListener('scroll', debounce(storeScroll), { passive: true });
