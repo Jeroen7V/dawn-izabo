146818549f5ab8b26220381bad597052f0acb287
Jeroen Vermeylen <jeroen@vermeylen.org>
2022-05-30 23:23:35 +0200

    style header past point

--- assets/izabo.js
+++ assets/izabo.js
@@ -12,11 +12,13 @@ const debounce = (fn) => {
 
 const storeScroll = () => {
   document.documentElement.dataset.scroll = window.scrollY;
+  document.documentElement.dataset.banner = false;
   var scrollPoint = -1;
   if (document.getElementsByClassName("banner").length > 0) {
     var imageHeight = document.getElementsByClassName("banner")[0].clientHeight;
     var headerHeight = document.getElementsByTagName("sticky-header")[0].clientHeight;
     scrollPoint = imageHeight - headerHeight;
+    document.documentElement.dataset.banner = true;
   }
   if (window.scrollY > scrollPoint) {
     document.documentElement.dataset.pastheader = true;
